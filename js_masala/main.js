0761e91fe8266338ca0e12cb055b264eb19bd95a
SolihovAbdulloh <abdullasolikh@mail.com>
2024-10-01 16:34:24 +0500

    finish

---
+++ js_masala/main.js
@@ -0,0 +1,46 @@
+
+// let passwords = 'Absd1234'
+
+// password(passwords)
+
+// function password(passwords){
+
+//     const regEx = /^[A-Z a-z 0-9]{8,}$/
+
+//     const result = regEx.test(passwords)
+    
+//     console.log(result);
+// }
+
+// //1-masala
+
+// const email = '@gmail'  // true holat
+
+// const regEx = /^[a-z@]{6}$/
+
+// const result = regEx.test(email)
+
+// console.log(result);
+
+//2-masala
+
+// const tel = +998-90-990-95-81
+
+// const regEx =(/^\d{5}|-\d{3}|-\d{2}|$ ,+$1-$2-$3-$4/)
+
+// let result = regEx.test(tel)
+
+// console.log(result);
+
+
+// 3-masala
+
+// let vaqt = '24:42'
+
+// const regEx = /^[01]\d|2[0-3]:([0-5]\d)$/
+
+// let result = regEx.test(vaqt)
+// console.log(result);
+
+//4-masala
+
